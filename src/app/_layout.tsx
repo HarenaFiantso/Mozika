9c7d39fb61bdc03d49d86a17f399559117ddd1f3
FiantsoHarena <fiantsorav@gmail.com>
2025-03-11 12:26:57 +0300

    chore(code-style): prettier configuration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,13 @@
-import "@/global.css"
-import {Text, View} from "react-native";
+import { Text, View } from 'react-native';
+
+import '@/global.css';
 
 export default function RootLayout() {
-    return (
-        <View className="flex-1 justify-center items-center">
-            <Text className="underline text-2xl font-bold">Hello, this is the first test with Mozika</Text>
-        </View>
-    )
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold underline">
+        Hello, this is the first test with Mozika
+      </Text>
+    </View>
+  );
 }
