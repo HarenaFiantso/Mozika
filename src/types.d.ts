1c05fd3283713ef83950f28c62ce19940ca18c5d
tendryAxel <hei.tendry@gmail.com>
2025-03-11 21:43:55 +0300

    feat(not-implemented): some function for song

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,30 @@
-export type SongType = {
+class SongType {
   id: number;
   name: string;
   localPath: string;
-};
+
+  constructor(id: number, name: string, localPath: string) {
+    this.id = id;
+    this.name = name;
+    this.localPath = localPath;
+  }
+
+  play(): void {
+    throw new NotImplementedError();
+  }
+
+  pause(): void {
+    throw new NotImplementedError();
+  }
+
+  path_exist(): boolean {
+    throw new NotImplementedError();
+  }
+
+  get_album(): AlbumType {
+    throw new NotImplementedError();
+  }
+}
 
 export type AlbumType = {
   id: number;
