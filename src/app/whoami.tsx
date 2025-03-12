77a466fbc01e0768c2882580023fbd6b0b76334c
FiantsoHarena <fiantsorav@gmail.com>
2025-03-12 09:36:19 +0300

    feat: welcome screen implementation

---
+++ src/app/whoami.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function Whoami() {
+  // TODO: Not implemented
+  return (
+    <View className="flex-1 items-center justify-center bg-[#121111]">
+      <Text className="text-2xl font-bold text-white underline">Who am I?</Text>
+    </View>
+  );
+}
