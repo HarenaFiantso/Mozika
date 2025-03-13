a160f6cbe39b7f790db21ecf0cc0849b5c905cae
FiantsoHarena <fiantsorav@gmail.com>
2025-03-13 12:08:46 +0300

    feat: whoami screen implementation

--- src/app/whoami.tsx
+++ src/app/whoami.tsx
@@ -1,10 +1,42 @@
-import { Text, View } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function BackButton({ children, ...rest }: TouchableOpacityProps) {
+  return (
+    <TouchableOpacity className="self-start" {...rest}>
+      <MaterialIcons name="arrow-back" color="white" size={35} />
+      {children}
+    </TouchableOpacity>
+  );
+}
 
 export default function Whoami() {
-  // TODO: Not implemented
+  const navigation = useNavigation();
+
   return (
-    <View className="flex-1 items-center justify-center bg-[#121111]">
-      <Text className="text-2xl font-bold text-white underline">Who am I?</Text>
+    <View className="flex-1 items-center justify-center bg-[#000000]">
+      <View className="px-5 absolute top-20 left-0 z-[999]">
+        <BackButton onPress={navigation.goBack} />
+      </View>
+      <SafeAreaView className="flex-1 w-full justify-center items-center gap-5">
+        <Image source={require('assets/images/icon.png')} className="w-[15rem] h-[15rem]" />
+        <Text className="text-white font-bold text-4xl">Who you are?</Text>
+        <View
+          className="w-[90%] h-16 px-4 bg-[#1E1E2D] rounded-2xl border-2 border-gray-500 flex flex-row items-center">
+          <TextInput
+            className="flex-1 text-white font-bold text-base"
+            placeholder="Write your name here"
+            placeholderTextColor="#7B7B8B"
+          />
+        </View>
+        <TouchableOpacity
+          className="w-[90%] rounded-[4rem] bg-[#06A0B5] py-5 shadow-custom-cyan"
+        >
+          <Text className="text-center text-xl font-bold text-white">Submit</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 }
