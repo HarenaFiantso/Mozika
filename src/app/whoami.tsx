3b29cf67b15d0d5d8223ec8e1b3d7e6025e022f8
FiantsoHarena <fiantsorav@gmail.com>
2025-03-13 12:20:10 +0300

    chore: store the user name prompt using zustand

--- src/app/whoami.tsx
+++ src/app/whoami.tsx
@@ -2,6 +2,7 @@ import { Image, Text, TextInput, TouchableOpacity, TouchableOpacityProps, View }
 import { useNavigation } from '@react-navigation/native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useUserStore } from '@/stores';
 
 function BackButton({ children, ...rest }: TouchableOpacityProps) {
   return (
@@ -14,6 +15,12 @@ function BackButton({ children, ...rest }: TouchableOpacityProps) {
 
 export default function Whoami() {
   const navigation = useNavigation();
+  const { name, setName } = useUserStore();
+
+  const handleSubmit = () => {
+    console.log('User name:', name);
+    alert(`Hello, ${name}!`);
+  }
 
   return (
     <View className="flex-1 items-center justify-center bg-[#000000]">
@@ -29,10 +36,13 @@ export default function Whoami() {
             className="flex-1 text-white font-bold text-base"
             placeholder="Write your name here"
             placeholderTextColor="#7B7B8B"
+            value={name}
+            onChangeText={setName}
           />
         </View>
         <TouchableOpacity
           className="w-[90%] rounded-[4rem] bg-[#06A0B5] py-5 shadow-custom-cyan"
+          onPress={handleSubmit}
         >
           <Text className="text-center text-xl font-bold text-white">Submit</Text>
         </TouchableOpacity>
