b3e5718ac1518e8bdd961a6655459d59d0010f0a
FiantsoHarena <fiantsorav@gmail.com>
2025-03-13 14:16:50 +0300

    chore(navigation): create a new tab for navigation

--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -1,9 +1,29 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 
 export default function MozLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: 'Library',
+          headerShown: false,
+        }}
+      />
+    </Tabs>
   );
 }
