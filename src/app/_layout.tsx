ef9b689a8c612b119d19b8156fb978e313ce70de
FiantsoHarena <fiantsorav@gmail.com>
2025-03-15 21:15:57 +0300

    refactor: navigation structure reinforcement

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,45 +1,11 @@
-import { Pressable, View } from 'react-native';
+import { FloatingPlayer } from '@/components';
+import { Slot } from 'expo-router';
 
-import { FloatingPlayer, TabBar } from '@/components';
-import '@/global.css';
-import { Entypo, Ionicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
-
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <View className="flex-1">
-      <Tabs
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#000',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-          },
-          headerLeft: () => (
-            <Pressable className="mr-5 pl-5">
-              <Ionicons name="menu" size={20} color="gray" />
-            </Pressable>
-          ),
-          headerRight: () => (
-            <View className="flex-row gap-5">
-              <Pressable className="pr-5">
-                <Ionicons name="search" size={20} color="white" />
-              </Pressable>
-              <Pressable className="pr-5">
-                <Entypo name="dots-three-vertical" size={20} color="white" />
-              </Pressable>
-            </View>
-          ),
-        }}
-        tabBar={props => <TabBar {...props} />}
-      >
-        <Tabs.Screen name="index" options={{ title: 'Library' }} />
-        <Tabs.Screen name="playlist" options={{ title: 'Playlists' }} />
-      </Tabs>
+    <>
+      <Slot />
       <FloatingPlayer />
-    </View>
+    </>
   );
 }
