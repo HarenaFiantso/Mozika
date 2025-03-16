2509b4ea6d9a235fab37e0c510e553c2bb1c13b3
FiantsoHarena <fiantsorav@gmail.com>
2025-03-16 14:39:30 +0300

    chore: create custom hooks to get player bg, use the repeat mode & player volume

--- src/hooks/usePlayerBackground.ts
+++ src/hooks/usePlayerBackground.ts
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 
 import { getColors } from 'react-native-image-colors';
-import { IOSImageColors } from 'react-native-image-colors/build/types';
+import { AndroidImageColors } from 'react-native-image-colors/build/types';
 
 export const usePlayerBackground = (imageUrl: string) => {
-  const [imageColors, setImageColors] = useState<IOSImageColors | null>(null);
+  const [imageColors, setImageColors] = useState<AndroidImageColors | null>(null);
 
   useEffect(() => {
     getColors(imageUrl, {
       fallback: '#000',
       cache: true,
       key: imageUrl,
-    }).then((colors: any) => setImageColors(colors as IOSImageColors));
+    }).then((colors: any) => setImageColors(colors as AndroidImageColors));
   }, [imageUrl]);
 
   return { imageColors };
