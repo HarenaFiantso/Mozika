f22da9a8f5aea5a68e10ff227acdf918bd9cd929
FiantsoHarena <fiantsorav@gmail.com>
2025-03-16 12:37:42 +0300

    `feat: add usePlayerBackground hook to extract dominant colors from image`

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -1,5 +1,18 @@
-import { Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { usePlayerBackground } from '@/hooks/usePlayerBackground';
+import { useActiveTrack } from 'react-native-track-player';
+import { unknownTrackImageUri } from '@/constants/images';
 
 export default function Player() {
-  return <Text>Player screen</Text>;
+  const activeTrack = useActiveTrack();
+  const { imageColors } = usePlayerBackground(activeTrack?.artwork ?? unknownTrackImageUri);
+
+  return (
+    <LinearGradient
+      className="flex-1"
+      colors={imageColors ? [imageColors.background, imageColors.primary] : ["#000", "#000"]}
+    >
+
+    </LinearGradient>
+  );
 }
---
+++ src/hooks/usePlayerBackground.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import { getColors } from 'react-native-image-colors'
+import { IOSImageColors } from 'react-native-image-colors/build/types'
+
+export const usePlayerBackground = (imageUrl: string) => {
+  const [imageColors, setImageColors] = useState<IOSImageColors | null>(null)
+
+  useEffect(() => {
+    getColors(imageUrl, {
+      fallback: "#000",
+      cache: true,
+      key: imageUrl,
+    }).then((colors: any) => setImageColors(colors as IOSImageColors))
+  }, [imageUrl])
+
+  return { imageColors }
+}
